bc9cde9fa37a226d92df3c3991e8404192915380
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-17 15:49:22 +0800

    update main package

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,24 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"remote-buddies/server/internal/config"
 	"remote-buddies/server/internal/db"
 	"remote-buddies/server/internal/routes"
 
+	"github.com/gorilla/sessions"
 	"github.com/jackc/pgx/v5"
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/github"
 )
 
 func main() {
-	dbUrl := os.Getenv("DATABASE_URL")
-	fmt.Printf("%s\n", dbUrl)
+	config, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	conn, err := pgx.Connect(context.Background(), dbUrl)
+	conn, err := pgx.Connect(context.Background(), config.DATABASE_URL)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close(context.Background())
+
+	goth.UseProviders(github.New(config.GITHUB_CLIENT_ID, config.GITHUB_CLIENT_SECRET, config.GITHUB_CLIENT_CALLBCK, config.GITHUB_CLIENT_SCOPE))
+	gothic.Store = sessions.NewCookieStore([]byte(config.SESSION_SECRET))
 
 	query := db.New(conn)
 
