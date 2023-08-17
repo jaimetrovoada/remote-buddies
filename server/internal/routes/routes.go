fcb7fe037fa825f344638bb50174d9c973fe654c
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-17 15:48:29 +0800

    add auth routes and logger middleware

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -5,7 +5,9 @@ import (
 	"remote-buddies/server/internal/handlers"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func NewRouter(db *db.Queries) *fiber.App {
@@ -13,6 +15,7 @@ func NewRouter(db *db.Queries) *fiber.App {
 	app := fiber.New()
 
 	app.Use(cors.New())
+	app.Use(logger.New())
 
 	api := app.Group("/api")
 
@@ -21,5 +24,7 @@ func NewRouter(db *db.Queries) *fiber.App {
 	})
 	api.Post("/users/:user/location", handlers.UserLocationHandler)
 	api.Get("/nearby", handlers.NearbyHandler)
+	api.Get("/auth", adaptor.HTTPHandlerFunc(handlers.AuthHandler))
+	api.Get("/auth/callback", adaptor.HTTPHandlerFunc(handlers.AuthCallbackHandler))
 	return app
 }
