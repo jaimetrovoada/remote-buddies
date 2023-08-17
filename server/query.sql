00a860239f934f0e5b565f4a47ee11c45e216d34
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-17 15:45:25 +0800

    update queries

--- server/query.sql
+++ server/query.sql
@@ -8,3 +8,12 @@ INSERT INTO "Location" (coords)
 VALUES (ST_Point($1, $2, 4326))
 RETURNING *;
 
+-- name: CreateUser :one
+INSERT INTO "User" ("name", "email", "image", "updated_at")
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: CreateAccount :one
+INSERT INTO "Account" ("userId", "type", "provider", "providerAccountId", "refresh_token", "access_token", "token_type", "scope")
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *;
