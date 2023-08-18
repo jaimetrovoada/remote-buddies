9212b7f61208ed574a08dc8a20f9857d2255eab2
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-18 11:53:26 +0800

    cleanup auth handlers

--- server/internal/handlers/auth.go
+++ server/internal/handlers/auth.go
@@ -1,24 +1,23 @@
 package handlers
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
 	"net/http"
-	"remote-buddies/server/internal/config"
-	"remote-buddies/server/internal/db"
-	"time"
+	"net/url"
+	"remote-buddies/server/internal/utils"
 
-	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/markbates/goth/gothic"
 )
 
 func (s *Service) AuthHandler(res http.ResponseWriter, req *http.Request) {
 	// try to get the user without re-authenticating
-	if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
-		fmt.Printf("gothUser:\n%+v\n", gothUser)
+	url, _ := url.Parse("http://localhost:3000/")
+	qParams := url.Query()
+	if _, err := gothic.CompleteUserAuth(res, req); err == nil {
+		log.Error(err)
+		qParams.Add("error", "AuthError")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
 	} else {
 		gothic.BeginAuthHandler(res, req)
 	}
@@ -26,83 +25,49 @@ func (s *Service) AuthHandler(res http.ResponseWriter, req *http.Request) {
 
 func (s *Service) AuthCallbackHandler(res http.ResponseWriter, req *http.Request) {
 
+	url, _ := url.Parse("http://localhost:3000/")
+	qParams := url.Query()
+
 	user, err := gothic.CompleteUserAuth(res, req)
 	if err != nil {
-		fmt.Fprintln(res, err)
-		return
-	}
-	jsonData, err := json.Marshal(user)
-	if err != nil {
-		panic(err)
-	}
-	var buf bytes.Buffer
-	err = json.Indent(&buf, jsonData, "", "\t")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("user:\n%+v\n", buf.String())
-
-	name := pgtype.Text{}
-	email := pgtype.Text{}
-	image := pgtype.Text{}
-	updatedAt := pgtype.Timestamptz{}
-
-	name.Scan(user.Name)
-	email.Scan(user.Email)
-	image.Scan(user.AvatarURL)
-	updatedAt.Scan(time.Now())
-
-	dbUser := new(db.CreateUserParams)
-	dbUser.Email = email
-	dbUser.Name = name
-	dbUser.Image = image
-	dbUser.UpdatedAt = updatedAt
-
-	fmt.Println("dbuser", dbUser)
-
-	result, err := s.db.CreateUser(context.Background(), *dbUser)
-	if err != nil {
-		fmt.Println(err)
+		log.Error(err)
+		qParams.Add("error", "AuthError")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
 		return
 	}
 
-	config, err := config.LoadConfig(".")
+	exists, err := utils.CheckUserExists(user.Email, s.db)
 	if err != nil {
-		log.Fatal("cannot load config:", err)
+		log.Error(err)
+		qParams.Add("error", "AuthError")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
+		return
+	}
+	if exists {
+		qParams.Add("error", "UserExists")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
+		return
 	}
 
-	scope := pgtype.Text{}
-	accessToken := pgtype.Text{}
-	refreshToken := pgtype.Text{}
-	tokenType := pgtype.Text{}
-
-	scope.Scan(config.GITHUB_CLIENT_SCOPE)
-	accessToken.Scan(user.AccessToken)
-	refreshToken.Scan(user.RefreshToken)
-	tokenType.Scan("bearer")
-
-	account := new(db.CreateAccountParams)
-	account.UserId = fmt.Sprintf("%x", result.ID.Bytes)
-	account.Type = "oauth"
-	account.Provider = user.Provider
-	account.ProviderAccountId = user.UserID
-	account.Scope = scope
-	account.AccessToken = accessToken
-	account.RefreshToken = refreshToken
-	account.TokenType = tokenType
-
-	s.db.CreateAccount(context.Background(), *account)
-
-	cookie := &http.Cookie{
-		Name:     "oauth.session-token",
-		Value:    "cookie_value",
-		Path:     "/",
-		Domain:   "localhost",
-		Expires:  time.Now().Add(24 * time.Hour),
-		HttpOnly: true,
+	id, err := utils.CreateNewUser(user, s.db)
+	if err != nil {
+		log.Error(err)
+		qParams.Add("error", "AuthError")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
+		return
 	}
 
-	// Set the cookie in the HTTP response
-	http.SetCookie(res, cookie)
-	http.Redirect(res, req, "http://localhost:3000", http.StatusFound)
+	if err := utils.CreateNewAccount(id, user, s.db); err != nil {
+		log.Error(err)
+		qParams.Add("error", "AuthError")
+		url.RawQuery = qParams.Encode()
+		http.Redirect(res, req, url.String(), http.StatusFound)
+		return
+	}
+
+	http.Redirect(res, req, url.String(), http.StatusFound)
 }
