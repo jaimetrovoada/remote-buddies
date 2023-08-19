c1a481baeeb4f8ba359898ec392900237aaa6b9d
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-19 16:50:17 +0800

    rename utils -> services

--- server/internal/controllers/auth.go
+++ server/internal/controllers/auth.go
@@ -3,7 +3,8 @@ package controllers
 import (
 	"fmt"
 	"remote-buddies/server/internal/db"
-	"remote-buddies/server/internal/utils"
+	"remote-buddies/server/internal/errors"
+	"remote-buddies/server/internal/services"
 
 	"github.com/labstack/echo/v4"
 	"github.com/markbates/goth/gothic"
@@ -13,7 +14,7 @@ func (s *Service) AuthHandler(c echo.Context) error {
 	// try to get the user without re-authenticating
 	w, r := c.Response(), c.Request()
 	if _, err := gothic.CompleteUserAuth(w, r); err == nil {
-		return &utils.AuthError{Message: "AuthError", Err: err}
+		return &errors.AuthError{Message: "AuthError", Err: err}
 	} else {
 		gothic.BeginAuthHandler(w, r)
 	}
@@ -27,22 +28,22 @@ func (s *Service) AuthCallbackHandler(c echo.Context) error {
 	w, r := c.Response(), c.Request()
 	gUser, err := gothic.CompleteUserAuth(w, r)
 	if err != nil {
-		return &utils.AuthError{Message: "AuthError", Err: err}
+		return &errors.AuthError{Message: "AuthError", Err: err}
 	}
 
 	var user db.User
 
-	if qUser, err := utils.GetUser(gUser.Email, s.db); err != nil {
-		return &utils.AuthError{Message: "AuthError", Err: err}
+	if qUser, err := services.GetUser(gUser.Email, s.db); err != nil {
+		return &errors.AuthError{Message: "AuthError", Err: err}
 	} else {
 		if user.Email.String != "" {
 			user = qUser
-			return utils.LoginUser(user, url, c)
+			return services.LoginUser(user, url, c)
 		}
 	}
 
-	if qUser, err := utils.CreateNewUser(gUser, s.db); err != nil {
-		return &utils.AuthError{Message: "AuthError", Err: err}
+	if qUser, err := services.CreateNewUser(gUser, s.db); err != nil {
+		return &errors.AuthError{Message: "AuthError", Err: err}
 	} else {
 		if user.Email.String != "" {
 			user = qUser
@@ -50,10 +51,10 @@ func (s *Service) AuthCallbackHandler(c echo.Context) error {
 	}
 
 	id := fmt.Sprintf("%d", user.ID.Bytes)
-	if err := utils.CreateNewAccount(id, gUser, s.db); err != nil {
-		return &utils.AuthError{Message: "AuthError", Err: err}
+	if err := services.CreateNewAccount(id, gUser, s.db); err != nil {
+		return &errors.AuthError{Message: "AuthError", Err: err}
 	}
 
-	return utils.LoginUser(user, url, c)
+	return services.LoginUser(user, url, c)
 
 }
