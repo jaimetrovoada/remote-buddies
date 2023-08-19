0bd6670e7d268132b8bc00128d8512088b682e2e
jaimetrovoada <jaimetrovoada@gmail.com>
2023-08-19 16:19:45 +0800

    change nearby handler

--- server/internal/handlers/location.go
+++ server/internal/handlers/location.go
@@ -5,18 +5,28 @@ import (
 	"log"
 	"net/http"
 	"remote-buddies/server/internal/db"
+	"remote-buddies/server/internal/utils"
+	"strconv"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/labstack/echo/v4"
 )
 
 func (s *Service) NearbyHandler(c echo.Context) error {
+	radius := 100
 	lat := c.QueryParam("lat")
 	lon := c.QueryParam("lon")
-	// _radius := c.Query("radius")
-	params := new(db.ListLocationsParams)
+	radius, err := strconv.Atoi(c.QueryParam("radius"))
+	if err != nil {
+		radius = 100
+	}
+
+	params := new(db.ListNearbyUsersParams)
 	params.StMakepoint = lat
 	params.StMakepoint_2 = lon
-	locs, err := s.db.ListLocations(context.Background(), *params)
+	params.StDwithin = radius
+	locs, err := s.db.ListNearbyUsers(context.Background(), *params)
 
 	if err != nil {
 		log.Fatalf("Error reading locations: %v", err)
@@ -37,13 +47,20 @@ func (s *Service) UserLocationHandler(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "bad request")
 	}
 
-	params := new(db.CreateLocationParams)
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*utils.JwtCustomClaims)
+
+	email := pgtype.Text{}
+	email.Scan(claims.Email)
+
+	params := new(db.UpdateUserLocationParams)
 	params.StPoint = userLocation.Lat
 	params.StPoint_2 = userLocation.Lon
+	params.Email = email
 
-	s.db.CreateLocation(r.Context(), *params)
-
-	log.Printf("lat: %v, lon: %v", userLocation.Lat, userLocation.Lon)
+	if err := s.db.UpdateUserLocation(r.Context(), *params); err != nil {
+		return err
+	}
 
 	return c.NoContent(http.StatusOK)
 }
