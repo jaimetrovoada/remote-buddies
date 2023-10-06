ccf4f93c5c720ec2e5d314d1fa4498b6e66c439a
jaimetrovoada <jaimetrovoada@gmail.com>
2023-10-06 12:02:48 +0100

    feat: redo migrations
    merge account and user tables

---
+++ server/migrations/00001_init_schemas.sql
@@ -0,0 +1,44 @@
+-- +goose Up
+-- +goose StatementBegin
+
+-- createTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMPTZ(3),
+    "image" TEXT,
+    "coords" geometry(Point, 4326),
+    "interests" varchar ARRAY[5],
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL,
+    "oauth_type" TEXT,
+    "oauth_provider" TEXT,
+    "oauth_providerAccountId" TEXT NOT NULL,
+    "oauth_refresh_token" TEXT,
+    "oauth_access_token" TEXT,
+    "oauth_expires_at" INTEGER,
+    "oauth_token_type" TEXT,
+    "oauth_scope" TEXT,
+    "oauth_id_token" TEXT,
+    "oauth_session_state" TEXT,
+    "oauth_token_secret" TEXT,
+    "oauth_token" TEXT,
+    
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateUserIdIndex
+CREATE UNIQUE INDEX "UserId_key" ON "User"("id");
+
+-- CreateEmailIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateCoordsIndex
+-- CREATE INDEX "Coords_idx" ON "User" USING GIST ("coords");
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE "User";
+-- +goose StatementEnd
--- server/migrations/20230822121348_add_user_interests.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE "User" ADD COLUMN "interests" varchar ARRAY[5];
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE "User" DROP COLUMN "interests";
--- +goose StatementEnd
